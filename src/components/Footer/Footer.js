15236c1171fa08330bec4060694f572b3138d94a
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-03-11 15:11:26 +0530

    intial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href="https://github.com/Mohitsinghparmarg">
+    Designed By Mohit Singh Parmar
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,77 @@
+const header = {
+  "homepage": "https://Mohitsinghparmarg.github.io/Mohit_PortFolio",
+  title: "Mohit Singh Parmar",
+};
+
+const about = {
+  name: "Mohit Singh Parmar",
+  role: "Full Stack Developer",
+  description:
+    'A Full Stack Developer designs and implements both front-end and back-end solutions, ensuring seamless integration and functionality across applications.',
+  resume: 'https://drive.google.com/file/d/1Qu3t4PHcN2TeSpYogzy6alPDgiDAxdsF/view',
+  social: {
+    linkedin: "https://www.linkedin.com/in/mohit-singh-parmar-1ba948274/",
+    github: "https://github.com/Mohitsinghparmarg",
+  },
+};
+
+const projects = [
+  {
+    name: "NetFlixGpt",
+    description:
+      "NetflixGPT is a React-based AI movie recommender with GPT search, API integration, Redux, and secure, responsive UI for a seamless experience.",
+    stack: [
+      "JavaScript",
+      "Reactjs",
+      "TailwindCSS",
+      "Firebass",
+      "Redux",
+      "TMDB API",
+      "GPT API"
+    ],
+    sourceCode:
+      "https://github.com/Mohitsinghparmarg/NetflixGPT---The-Beginning",
+  },
+  {
+    name: "DevSpark",
+    description:
+      "DevSpark is a social app with real-time chat, profile updates, JWT auth, RESTful APIs, and optimized database performance.",
+    stack: ["JavaScript","Node.js", "Express.js", "MongoDB", "REST API", "Pagination", "JWT Authentication"],
+    sourceCode: "https://github.com/Mohitsinghparmarg/DevSpark",
+  },
+  {
+    name: "FlavorFlow",
+    description:
+      "FlavorFlow is a food ordering app linking users to restaurants for quick delivery and a smooth experience, with plans to enhance UX and expand options.",
+    stack: ["JavaScript","Reactjs", "TailwindCSS", "Redux"],
+    sourceCode: "https://github.com/Mohitsinghparmarg/FlavorFlow",
+  },
+  {
+    name: "iCompressor",
+    description:
+      "I built a C++ CLI text compression tool with a 40% compression ratio, using a Trie for speed and custom dictionaries for flexibility.",
+    stack: ["C++", "Trie Data Structure", "Command-Line Application", "Header Files"],
+    sourceCode: "https://github.com/Mohitsinghparmarg/iCompressor",
+  },
+  {
+    name: "Sentiment Analysis",
+    description:
+      "This Python tool analyzes text sentiment as positive, negative, or neutral using NLTK and Scikit-learn, offering insights into opinions and feedback.",
+    stack: ["Python", "NumPy", "NLTK", "Keras"],
+    sourceCode: "https://github.com/Mohitsinghparmarg/sentiment_analysis",
+  },
+
+];
+
+const skills = [
+  "C/C++", "Java", "Python", "JavaScript", "TypeScript", "Golang", "SQL (PostgreSQL, MySQL)","HTML/CSS", "Bootstrap", "Tailwind CSS", "React.js", "Redux", "Next.js",
+"Node.js", "Express.js", "MongoDB", "RESTful APIs", "EJS",
+"Material UI", "JWT",
+"Linux", "Git", "GitHub"
+];
+
+const contact = {
+  email: "mohitparmar1501@gmail.com@gmail.com",
+};
+
+export { header, about, projects, skills, contact };
